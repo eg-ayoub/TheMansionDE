c721c8f7b7a07ad7e12b844b63d342bf1cf9ad95
ayoub nasr <mrtheshotgunner97@gmail.com>
2019-10-18 09:35:59 +0200

    Pumpkine mechanic and animation.
    Modified pumpkine animation so we only scale the sprite. This avoids
    scaling the colliders during contact.
    Added "PlayerBounce" functionality to Pumpkine script.
    
    Related to #21

---
+++ Assets/Scripts/Environment/Platforms/Pumpkine.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using Player;
+using Player.Movement;
+namespace Environment.Platforms
+{
+    public class Pumpkine : Platform
+    {
+        Animator animator;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+        const float pinnacle = 1000.0f;
+
+        public override void PlayerOff()
+        {
+            // ! do nothing
+        }
+
+        public override void PlayerOn()
+        {
+            animator.SetTrigger("Bounce");
+        }
+
+        public void BouncePlayer()
+        {
+            PlayerInstanciationScript.movementModifier.TrampolineJump(pinnacle);
+        }
+
+
+    }
+}
