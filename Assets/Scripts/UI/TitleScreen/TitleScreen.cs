a78e16a6862d1de2d43783f41354ca5384432614
ayoub nasr <mrtheshotgunner97@gmail.com>
2019-10-25 18:20:50 +0200

    Work on title screen (prequel)
    Commiting first work on title screen to pull.

---
+++ Assets/Scripts/UI/TitleScreen/TitleScreen.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace UI.TitleScreen
+{
+    public class TitleScreen : MonoBehaviour
+    {
+        public static TitleScreen titleScreen;
+        Animator animator;
+
+        private void Awake()
+        {
+            if (titleScreen == null)
+            {
+                titleScreen = this;
+                DontDestroyOnLoad(this);
+            }
+            else if (titleScreen != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+            animator.SetTrigger("Start");
+        }
+
+        public void Kill()
+        {
+            // * un-pause game 
+            animator.SetTrigger("Kill");
+        }
+    }
+}
